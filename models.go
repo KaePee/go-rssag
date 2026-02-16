4fca4b938ef8f862668fcceb588a5aa1c1fff6ad
kaepee <obedpksomuah@gmail.com>
2026-02-16 10:56:00 +0000

    updates

--- models.go
+++ models.go
@@ -22,3 +22,59 @@ func databaseUserToUser(dbUser db.User) User {
 		ApiKey:    dbUser.ApiKey,
 	}
 }
+
+type Feed struct {
+	ID        pgtype.UUID      `json:"id"`
+	CreatedAt pgtype.Timestamp `json:"created_at"`
+	UpdatedAt pgtype.Timestamp `json:"updated_at"`
+	Name      string           `json:"name"`
+	Url       string           `json:"url"`
+	UserId    string           `json:"user_id"`
+}
+
+type FeedFollow struct {
+	ID        pgtype.UUID      `json:"id"`
+	CreatedAt pgtype.Timestamp `json:"created_at"`
+	UpdatedAt pgtype.Timestamp `json:"updated_at"`
+	UserId    pgtype.UUID      `json:"user_id"`
+	FeedId    pgtype.UUID      `json:"feed_id"`
+}
+
+func databaseFeedtoFeed(dbFeed db.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		Url:       dbFeed.Url.String,
+		UserId:    dbFeed.UserID.String(),
+	}
+}
+
+func databaseFeedstoFeeds(dbFeeds []db.Feed) []Feed {
+	feeds := []Feed{}
+
+	for _, dbFeed := range dbFeeds {
+		feeds = append(feeds, databaseFeedtoFeed(dbFeed))
+	}
+	return feeds
+}
+
+func databaseFeedFollowToFeedFollow(dbFeedFollow db.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        dbFeedFollow.ID,
+		CreatedAt: dbFeedFollow.CreatedAt,
+		UpdatedAt: dbFeedFollow.UpdatedAt,
+		UserId:    dbFeedFollow.UserID,
+		FeedId:    dbFeedFollow.FeedID,
+	}
+}
+
+func databaseFeedsFollowstoFeedsFollows(dbFeedFollows []db.FeedFollow) []FeedFollow {
+	feedFollows := []FeedFollow{}
+
+	for _, dbFeedFollow := range dbFeedFollows {
+		feedFollows = append(feedFollows, databaseFeedFollowToFeedFollow(dbFeedFollow))
+	}
+	return feedFollows
+}
